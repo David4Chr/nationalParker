284f1ecbdd9ca457c6e3e4b3a4b2dc709d31e642
David4Chr <avdiakovd@gmail.com>
2024-02-22 18:19:06 +0100

    Bytte tillbaka till /nationalparker

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ db.once('open', function (callback) {
 console.log("Kopplingen lyckades!");
 });
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
-app.use('/', nationalparkerRouter);
-
+app.use(cors()); 
+app.use('/', indexRouter);
+app.use('/nationalparker', nationalparkerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
